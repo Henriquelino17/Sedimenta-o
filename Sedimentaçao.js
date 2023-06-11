1fe2affea0ecadf4c26567d97516be4bca42aa6c
Henriquelino17 <90516324+Henriquelino17@users.noreply.github.com>
2023-06-11 15:09:35 -0300

    Sedimentaçao.js

--- Sedimentaçao.js
+++ Sedimentaçao.js
@@ -1,4 +1,27 @@
 function Cc() {
+    
+  
+    var Check = document.getElementById("scales"); 
+    var Check2 = document.getElementById("horns"); 
+    //var Check =  10 ;
+    //var Check2 = 20 ;
+    //console.log (Check);
+    //console.log (Check2);
+    if (Check2.checked){
+      var Tempo = document.getElementById("Tempo").value;
+    
+      var Sedimentação = 20/Tempo;
+      var Sedimentação_2 = Sedimentação * 60;
+    
+    
+      var round = Sedimentação_2.toFixed(2);
+      var elemento = document.getElementById("C"); 
+    //console.log(round);
+    elemento.textContent = "A Sedimentação está em  "+ round + " Cm/min";
+    }else{
+      
+
+  
     var Tempo = document.getElementById("Tempo").value;
     
     var Sedimentação = 10/Tempo;
@@ -7,9 +30,9 @@ function Cc() {
     
     var round = Sedimentação_2.toFixed(2);
     var elemento = document.getElementById("C"); 
-    console.log(round);
+    //console.log(round);
     elemento.textContent = "A Sedimentação está em  "+ round + " Cm/min";
     
     
   }
- 
+}
